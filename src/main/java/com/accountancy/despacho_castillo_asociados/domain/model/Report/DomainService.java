3d4472dd76401a71fc821215f521cc04a214eb14
Kenny Kalet Meléndez Arce <kaletmelenar24@gmail.com>
2026-01-28 12:49:54 -0600

    Feat(API): Create the domains for reports and report categories

---
+++ src/main/java/com/accountancy/despacho_castillo_asociados/domain/model/Report/DomainService.java
@@ -0,0 +1,86 @@
+package com.accountancy.despacho_castillo_asociados.domain.model.Report;
+
+import java.awt.*;
+import java.net.URL;
+import java.time.LocalDate;
+
+public class DomainService {
+
+    private int id;
+    private String title;
+    private String description;
+    private URL image;
+    private String category;
+    private LocalDate date;
+    private boolean active;
+
+    public DomainService(int id, String title, String description, URL image, String category, LocalDate date, boolean active) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.image = image;
+        this.category = category;
+        this.date = date;
+        this.active = active;
+    }
+
+    public DomainService(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        description = description;
+    }
+
+    public URL getImage() {
+        return image;
+    }
+
+    public void setImage(URL image) {
+        this.image = image;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
